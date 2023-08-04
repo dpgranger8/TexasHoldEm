5463c4b0a103038c56724242fcce77af69681d15
David Granger <dpgranger8@gmail.com>
2023-08-03 21:15:41 -0600

    Initial Commit

---
+++ TexasHoldEm/TexasHoldEmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TexasHoldEmApp.swift
+//  TexasHoldEm
+//
+//  Created by David Granger on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TexasHoldEmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
